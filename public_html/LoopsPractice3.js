1c6d0241f96115529dd36225b1434ffc5167808f
MilleKel <MilleKel@TS31528.NIACC.EDU>
2020-10-12 18:57:54 -0500

    Initial commit

---
+++ public_html/LoopsPractice3.js
@@ -0,0 +1,6 @@
+/* 
+ * Display the first 10 numbers in the fibonacci sequence
+ * 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+ */
+
+document.write("This program displays the first 10 numbers in the fibonacci sequence.");
